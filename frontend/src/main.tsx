83912625f651e71fb72180b0e9fa277abcffb1e4
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-07-09 16:47:58 -0400

    skelton set up, adding protected routing

---
+++ frontend/src/main.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Import the layouts
+import RootLayout from './layouts/root-layout'
+import DashboardLayout from './layouts/dashboard-layout'
+
+// Import the components
+import IndexPage from './routes'
+import ContactPage from './routes/contact'
+import SignInPage from './routes/sign-in'
+import SignUpPage from './routes/sign-up'
+import DashboardPage from './routes/dashboard'
+import DashboardExtension from './routes/dashboard.extension'
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/contact", element: <ContactPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: "dashboard",
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+          { path: "/dashboard/extension", element: <DashboardExtension /> }
+        ]
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ frontend/src/routes/sign-up.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { SignUp } from "@clerk/clerk-react"
+
+export default function SignUpPage() {
+    return <SignUp path="/sign-up" />;
+}
