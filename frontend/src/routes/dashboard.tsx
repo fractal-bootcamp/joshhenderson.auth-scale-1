d02a65116c5e948754d12de7c60cf8ba2b89db3f
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-07-09 18:57:36 -0400

    authorize req still not working

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
+
+const url = "http://localhost:3000"
+
+const { getToken } = useAuth()
+
+export async function getWorld(args) {
+    return fetch((args), {
+        headers: { Authorization: `Bearer ${await getToken()}` }
+    }).then(res => res.json());
+}
 
 export default function DashboardPage() {
+    const [world, setWorld] = useState("")
+    useEffect(() => {
+        const theWorld = getWorld(url)
+        setWorld(theWorld)
+    }, [])
+
     return (
         <>
             <h1>Dashboard page</h1>
             <p>This is a protected page.</p>
-
+            <p>{world}</p>
             <ul>
                 <li><Link to="/dashboard/extension">Dashboard-Extension</Link></li>
                 <li><Link to="/">Return to index</Link></li>
